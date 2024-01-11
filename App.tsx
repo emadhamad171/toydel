a04cb132fc7b90285b55b84bf449a1534b3bbb3e
emadhamad <dusikeh@gmail.com>
2024-01-11 19:09:38 +0200

    Profile screen added

--- App.tsx
+++ App.tsx
@@ -6,30 +6,30 @@ import * as firebase_auth from "firebase/auth";
 import Toast from "react-native-toast-message";
 import {onAuthStateChanged} from "firebase/auth";
 import {auth} from "./src/firebase";
+import Profile from "./src/screens/Profile";
 
 export default function App() {
-  const [user, setUser] = useState<firebase_auth.User>();
+    const [user, setUser] = useState<firebase_auth.User>();
     useEffect(() => {
         onAuthStateChanged(auth, (userInstance) => {
             !!userInstance && setUser(userInstance);
-            !!userInstance && Toast.show({type: 'success', text1:'Log in successfully!'});
+            !!userInstance && Toast.show({type: 'success', text1: 'Log in successfully!', text2: '', position: 'top', swipeable: true});
         });
     }, []);
 
-  return (
-      <View style={styles.container}>
-          {!user && <Authorization />}
-          <StatusBar style="auto" />
-          <Toast />
-      </View>
-  );
+    return (
+        <View style={styles.container}>
+            {!user && <Authorization />}
+            {user && <Profile user={user} setUser={setUser}/> }
+            <StatusBar style="auto" hidden={true} />
+            <Toast />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
 });

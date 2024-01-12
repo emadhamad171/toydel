15b54eb83b4970e83ce2279c7d02ac4efffffc85
emadhamad <dusikeh@gmail.com>
2024-01-12 17:05:22 +0200

    Review modal added.

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -12,8 +12,6 @@ import {StatusBar} from "expo-status-bar";
 import Input from "../components/Input";
 import {styles} from "../styles/authorization";
 import ContinueButton from "../components/ContinueButton";
-import App from "../../App";
-import getAppKeys = AppRegistry.getAppKeys;
 
 const updateName = async ({user, name}) => {
     await updateProfile(user,{displayName: name});
@@ -29,13 +27,15 @@ const updateImage = async({userInstance, user})=>{
             quality: 0.1,
         };
         const res = await launchImageLibrary(options);
-        const uri = res?.assets && res.assets[0].base64;
-       await updateProfile(userInstance, {photoURL: "https://freight.cargo.site/t/original/i/c511067eb5c64bbe81b3c66c11edbd2533233dcaff898525f899d158680a76ce/gasmask_someart_friendly_faces.jpg"});
+        // const uri = res?.assets && res.assets[0].base64;
+       // await updateProfile(userInstance, {photoURL: "https://freight.cargo.site/t/original/i/c511067eb5c64bbe81b3c66c11edbd2533233dcaff898525f899d158680a76ce/gasmask_someart_friendly_faces.jpg"});
         // user.photoURL=uri;
     } catch (e){
         console.log(e);
     }
 }
+
+const Star = ({color = '#e5b600', size=16, starName = 'star'}) =><Icon name={starName} color={color} size={size} />
 const HeaderText = ({text,style, ...props}:{text:string, style?:any})=>{
 
     return <Text style={style || {...styles.subHeaderText, marginBottom: 4}} {...props}>
@@ -200,6 +200,50 @@ const InfoModal = ({props}) =>{
     </ScrollView>
 }
 
+const ReviewComponent = ({item})=>{
+    const filledStars = new Array(Math.floor(item.rate)).fill(<Star />);
+    const starRate = [...filledStars]
+    const unfiledStars = new Array(5-Math.round(item.rate)).fill(<Star starName={'star-outline'} />);
+    if(item.rate%1) starRate.push(<Star starName={'star-half-full'}/>)
+    starRate.push(unfiledStars);
+return <View style={{flexDirection: 'row', gap :18, marginTop: 24}}>
+    <Image source={{uri: item.photo}} style={{borderRadius: 50, width: 50, height: 50}} />
+    <View style={{backgroundColor:'#e0e0e0', borderRadius: 30, padding: 15, width: 270, marginRight: 1000}}>
+        <Text style={{color: '#5d5d5d'}}>{item.date}</Text>
+        <Text style={{...styles.subHeaderText, marginBottom: 2}}>{item.name}</Text>
+        <Text>{starRate}</Text>
+        <Text>{item.text}</Text>
+    </View>
+</View>
+}
+const ReviewModal = ()=>{
+    const reviews =[
+        {
+            date: '2023/03/16',
+            photo: 'https://www.siasat.com/wp-content/uploads/2022/02/IMG_18022022_181903_1200_x_900_pixel.jpg',
+            rate: 4.5,
+            name: 'Huan',
+            text: 'Very good!'
+        },
+        {
+            date: '2023/03/19',
+            photo: 'https://www.siasat.com/wp-content/uploads/2022/02/IMG_18022022_181903_1200_x_900_pixel.jpg',
+            rate: 4,
+            name: 'Samir',
+            text: 'I was a poor and hungry man, but after this app came into my life, I became a multi-millionaire! Thank you!'
+        },
+        {
+            date: '2023/03/15',
+            photo: 'https://www.siasat.com/wp-content/uploads/2022/02/IMG_18022022_181903_1200_x_900_pixel.jpg',
+            rate: 5,
+            name: 'Roman',
+            text: 'Very nice!'
+        }
+    ]
+    return <View style={{marginTop: 24}}>
+        <FlatList data={reviews} renderItem={({item})=><ReviewComponent item={item}/>} />
+    </View>
+}
 function WrapperComponent({ItemModal, setModal, modalName}) {
     return (
             <Modal propagateSwipe style={{padding: 0, margin: 0, flex:1}} swipeDirection="left" onSwipeComplete={()=>{setModal(null)}} animationInTiming={600} animationOutTiming={500} animationOut={'slideOutDown'} coverScreen={false} backdropOpacity={0} isVisible={!!ItemModal} onBackdropPress={() => setModal(null)}>
@@ -262,7 +306,7 @@ const Profile = ({user, setUser}) => {
                 setModalName("Faq");
             }} placeholder={"FAQ"} />
             <UserButton icon={'star'} iconSize={24} onPressAction={()=>{
-                setModal(()=>{return ()=><FaqModal />})
+                setModal(()=>{return ()=><ReviewModal />})
                 setModalName("Reviews");
             }} placeholder={"Reviews"} />
         </View>

1d1c8739b110a5d6e19988cc62e2463af36b28b4
emadhamad <dusikeh@gmail.com>
2024-04-24 05:45:08 +0300

    hotfixes

--- src/screens/resetPassword/index.tsx
+++ src/screens/resetPassword/index.tsx
@@ -15,72 +15,11 @@ import {confirmPasswordReset} from "firebase/auth";
 import Toast from "react-native-toast-message";
 import {AnimatePresence, MotiView} from "moti";
 import Icon from "react-native-vector-icons/Entypo";
+import {AnimatedView, Circle, Cloud, Star} from "./ui";
 
 enum advices {
     default = 'Обидва паролі повинні збігатися',
     protect = 'Пароль складатись що найменше з 6 символів, 1 великої, 1 малої літер та 1 цифри'
-}
-
-const AnimatedView = ({children, duration = 400, style={}, from, exit, exitDuration=600, animate}: any) => {
-    return <MotiView
-        from={{
-        opacity: 0,
-        ...from
-        }}
-        animate={{
-            opacity: 1,
-            scale: 1,
-            ...animate
-        }}
-        exit={{
-            opacity: 0.5,
-            ...exit
-        }}
-        exitTransition={{
-            type: 'timing',
-            duration: exitDuration,
-        }}
-        transition={{
-            type: 'timing',
-            duration: duration
-        }}
-        style={style}
-    >
-        {
-            children
-        }
-    </MotiView>
-}
-const Circle = ({children}: {children: any}) => <AnimatedView
-    duration={1000}
-    from={{
-        scale: 0,
-    }}
-    exitDuration={1600}
-    style={{
-        width: normalize(82),
-        height: normalize(82),
-        borderRadius: 50,
-        position: 'relative',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#7065EB',
-        zIndex: 666,
-    }}
->
-    {children}
-</AnimatedView>;
-
-const Star = ({left, top, duration = 1000}) => {
-    return <AnimatedView duration={duration} style={{position: 'absolute',left: left, top: top}}>
-        <Image source={require('../../../assets/Star.png')} />
-    </AnimatedView>
-}
-
-const Cloud = ({left, top,width=normalize(164), duration = 1000,fromLeft=false}) => {
-    return <AnimatedView duration={duration} from={{translateX: fromLeft ? -windowWidth : windowWidth}} animate={{translateX: 0}} style={{position: 'absolute',left: left, top: top}}>
-        <Image style={{width: width, objectFit: 'contain'}} source={require('../../../assets/Cloud.png')} />
-    </AnimatedView>
 }
 const ResetPasswordScreen = () => {
     const dispatch = useAppDispatch();
---
+++ src/screens/resetPassword/ui/index.tsx
@@ -0,0 +1,65 @@
+import {MotiView} from "moti";
+import {normalize, windowWidth} from "@shared";
+import {Image} from "react-native";
+
+export const AnimatedView = ({children, duration = 400, style={}, from, exit, exitDuration=600, animate}: any) => {
+    return <MotiView
+        from={{
+            opacity: 0,
+            ...from
+        }}
+        animate={{
+            opacity: 1,
+            scale: 1,
+            ...animate
+        }}
+        exit={{
+            opacity: 0.5,
+            ...exit
+        }}
+        exitTransition={{
+            type: 'timing',
+            duration: exitDuration,
+        }}
+        transition={{
+            type: 'timing',
+            duration: duration
+        }}
+        style={style}
+    >
+        {
+            children
+        }
+    </MotiView>
+}
+export const Circle = ({children}: {children: any}) => <AnimatedView
+    duration={1000}
+    from={{
+        scale: 0,
+    }}
+    exitDuration={1600}
+    style={{
+        width: normalize(82),
+        height: normalize(82),
+        borderRadius: 50,
+        position: 'relative',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#7065EB',
+        zIndex: 666,
+    }}
+>
+    {children}
+</AnimatedView>;
+
+export const Star = ({left, top, duration = 1000}) => {
+    return <AnimatedView duration={duration} style={{position: 'absolute',left: left, top: top}}>
+        <Image source={require('../../../../assets/Star.png')} />
+    </AnimatedView>
+}
+
+export const Cloud = ({left, top,width=normalize(164), duration = 1000,fromLeft=false}) => {
+    return <AnimatedView duration={duration} from={{translateX: fromLeft ? -windowWidth : windowWidth}} animate={{translateX: 0}} style={{position: 'absolute',left: left, top: top}}>
+        <Image style={{width: width, objectFit: 'contain'}} source={require('../../../../assets/Cloud.png')} />
+    </AnimatedView>
+}
